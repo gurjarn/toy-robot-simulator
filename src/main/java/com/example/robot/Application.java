eb06c1ec2d2cfcec1ec768f3e14547762dcdeaa5
usr2312 <noreply@usr2312.com>
2019-12-09 12:22:04 +1100

    TASK: Initial project structure

---
+++ src/main/java/com/example/robot/Application.java
@@ -0,0 +1,12 @@
+package com.example.robot;
+
+/**
+ * Application launch
+ */
+public class Application {
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
