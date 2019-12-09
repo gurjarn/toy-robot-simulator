a4d6202ddfd60be0579253381e4f47cd2b812646
usr2312 <noreply@usr2312.com>
2019-12-09 13:35:06 +1100

    Add user input test cases

---
+++ src/test/java/com/example/robot/ApplicationTest.java
@@ -0,0 +1,44 @@
+package com.example.robot;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+
+    /**
+     * User does valid selection
+     */
+    @Test
+    public void testUserInputValidSelection()
+    {
+        Assertions.fail();
+    }
+
+    /**
+     * User enters invalid text. e.g. PLACE x,x,NORTHEAST
+     */
+    @Test
+    public void testUserInputInvalidText()
+    {
+        Assertions.fail();
+    }
+
+    /**
+     * User does invalid selection
+     */
+    @Test
+    public void testUserInputInvalidSelection()
+    {
+        Assertions.fail();
+    }
+
+    /**
+     * user chooses file input option
+     */
+    @Test
+    public void testUserInputFile()
+    {
+        Assertions.fail();
+    }
+
+}
