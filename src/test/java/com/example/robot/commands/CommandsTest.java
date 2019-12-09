b0011e85f47542f711f236228fe2ff9b309155e1
usr2312 <noreply@usr2312.com>
2019-12-09 17:11:34 +1100

    Modify test case for PLACE command

--- src/test/java/com/example/robot/commands/CommandsTest.java
+++ src/test/java/com/example/robot/commands/CommandsTest.java
@@ -1,5 +1,8 @@
 package com.example.robot.commands;
 
+import com.example.robot.commands.data.CommandException;
+import com.example.robot.commands.data.StatusContext;
+import com.example.robot.commands.utils.CommandUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
@@ -21,7 +24,16 @@ public class CommandsTest {
      */
     @ParameterizedTest
     @CsvFileSource(resources = "/command_test.csv")
-    public void testOperation(String expected, String commands){
-        Assertions.fail();
+    public void testOperation(String expected, String commands) {
+
+        StatusContext context = new StatusContext();
+        String actual;
+        try {
+            context = CommandUtils.executeCommands(context,commands);
+            actual = context.toString();
+        } catch (CommandException e) {
+            actual = e.getMessage();
+        }
+        Assertions.assertEquals(expected,actual);
     }
 }
