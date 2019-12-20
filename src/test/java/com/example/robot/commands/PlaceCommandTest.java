c19c7fbace34d68ae52c88cb2a03ab552ba7749a
Gurjar <Niraj.Gurjar@team.telstra.com>
2019-12-20 14:41:49 +1100

    TASK: Refactor unit tests to command tests

---
+++ src/test/java/com/example/robot/commands/PlaceCommandTest.java
@@ -0,0 +1,40 @@
+package com.example.robot.commands;
+
+import com.example.robot.commands.data.CommandException;
+import com.example.robot.commands.data.StatusContext;
+import com.example.robot.commands.service.CommandExecutor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+/**
+ * This class is to test sequence of PLACE commands and assert the context
+ * The test class is parameterized and takes its input from resource file src/test/resources/place_command_test.csv
+ * It takes two values
+ * "expected message", "new line separated list of commands"
+ * Please cover list of commands in double quotes to treat it as a row in CSV parsing
+ *
+ */
+public class PlaceCommandTest {
+
+    /**
+     *
+     * @param expected: expected message from the execution of commands. This is the first parameter
+     * @param commands: list of commands to be executed. This is second parameter
+     */
+    @ParameterizedTest
+    @CsvFileSource(resources = "/place_command_test.csv")
+    public void testOperation(String expected, String commands) {
+
+        StatusContext context = new StatusContext();
+        String actual;
+        try {
+            CommandExecutor executor = new CommandExecutor();
+            context = executor.executeCommands(context,commands);
+            actual = context.toString();
+        } catch (CommandException e) {
+            actual = e.getMessage();
+        }
+        Assertions.assertEquals(expected,actual);
+    }
+}
